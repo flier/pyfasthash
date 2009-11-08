0d2ebc47f7f9d90600e306c319481b18b6131f23
Flier Lu <flier.lu@gmail.com>
2009-11-08 02:15:07 +0000

    define uint64_t with unsigned long long, and make GCC happy

--- src/MurmurHash/MurmurHash2_64.cpp
+++ src/MurmurHash/MurmurHash2_64.cpp
@@ -4,7 +4,7 @@
 // The same caveats as 32-bit MurmurHash2 apply here - beware of alignment 
 // and endian-ness issues if used across multiple platforms.
 
-typedef unsigned __int64 uint64_t;
+typedef unsigned long long uint64_t;
 
 // 64-bit hash for 64-bit platforms
 
