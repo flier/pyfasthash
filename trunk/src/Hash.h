81902400d3c700ac33c96cf8cabcd279f2102eb4
flier.lu <flier.lu@3c9281d8-cbc6-11de-a774-41d3a1f294e1>
2012-09-27 05:43:57 +0000

    support to build at Win64

--- trunk/src/Hash.h
+++ trunk/src/Hash.h
@@ -3,7 +3,7 @@
 #if defined(_MSC_VER)
   typedef unsigned char uint8_t;
   typedef unsigned short uint16_t;
-  typedef unsigned long uint32_t;
+  typedef unsigned int uint32_t;
   typedef unsigned __int64 uint64_t;  
 #else
   #include <stdint.h>     /* defines uint32_t etc */
