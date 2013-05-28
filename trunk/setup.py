9f60913a6e70fb9e04e154cba6a82aa7a436888a
flier.lu@gmail.com <flier.lu@gmail.com@3c9281d8-cbc6-11de-a774-41d3a1f294e1>
2013-05-28 14:26:53 +0000

    porting to Python 3.3

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,16 +1,28 @@
 #!/usr/bin/env python
-import ez_setup
-ez_setup.use_setuptools()
+# -*- coding: utf-8 -*-
+from __future__ import with_statement
+from __future__ import print_function
 
-import sys, os, os.path
+import sys
+import os
+import math
+
+is_py3k = sys.version_info[0] > 2
+
+if is_py3k:
+    import distribute_setup
+    distribute_setup.use_setuptools()
+else:
+    import ez_setup
+    ez_setup.use_setuptools()
 
 from setuptools import setup, Extension
 
 libraries = {
-    'fnv' : ['hash_32.c', 'hash_32a.c', 'hash_64.c', 'hash_64a.c'],
-    'MurmurHash' : ['MurmurHash1.cpp', 'MurmurHash2.cpp', 'MurmurHash3.cpp'],
-    'lookup3' : ['lookup3.c'],
-    'SuperFastHash' : ['SuperFastHash.c'],
+    'fnv': ['hash_32.c', 'hash_32a.c', 'hash_64.c', 'hash_64a.c'],
+    'MurmurHash': ['MurmurHash1.cpp', 'MurmurHash2.cpp', 'MurmurHash3.cpp'],
+    'lookup3': ['lookup3.c'],
+    'SuperFastHash': ['SuperFastHash.c'],
 }
 
 source_files = [os.path.join('src', file) for file in ['Hash.cpp']]
@@ -46,25 +58,37 @@ if os.name == "nt":
     extra_compile_args += ["/O2", "/GL", "/MT", "/EHsc", "/Gy", "/Zi"]
     extra_link_args += ["/DLL", "/OPT:REF", "/OPT:ICF", "/MACHINE:X64" if is_64bit else "/MACHINE:X86"]
 elif os.name == "posix" and sys.platform == "darwin":
+    is_64bit = math.trunc(math.ceil(math.log(sys.maxsize, 2)) + 1) == 64
+    include_dirs += [
+        '/opt/local/include',
+        '/usr/local/include'
+    ]
     libraries += ["boost_python-mt"]
 elif os.name == "posix":
     libraries += ["boost_python", "rt"]
 
 pyhash = Extension(name="_pyhash",
-                   sources = source_files,
-                   define_macros = macros,
-                   include_dirs = include_dirs,
-                   library_dirs = library_dirs,
-                   libraries = libraries,
-                   extra_compile_args = extra_compile_args,
-                   extra_link_args = extra_link_args,
+                   sources=source_files,
+                   define_macros=macros,
+                   include_dirs=include_dirs,
+                   library_dirs=library_dirs,
+                   libraries=libraries,
+                   extra_compile_args=extra_compile_args,
+                   extra_link_args=extra_link_args,
                    )
 
+extra = {}
+
+if is_py3k:
+    extra.update({
+        'use_2to3': True
+    })
 
 setup(name='pyhash',
-    version='0.4.2',
+    version='0.5.0',
     description='Python Non-cryptographic Hash Library',
-    long_description="pyhash is a python non-cryptographic hash library, including FNV1, MurmurHash1/2/3, lookup3, SuperFastHash, etc",
+    long_description="pyhash is a python non-cryptographic hash library, "
+                     "including FNV1, MurmurHash1/2/3, lookup3, SuperFastHash, etc",
     platforms="x86",
     author='Flier Lu',
     author_email='flier.lu@gmail.com',
@@ -83,8 +107,9 @@ setup(name='pyhash',
         'Operating System :: POSIX', 
         'Programming Language :: C++',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Utilities'
-    ])
+    ], **extra)
