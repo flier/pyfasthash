cccb591fd9d0dd7e3baeb301a8b92645ac9d28e0
Flier Lu <flier.lu@gmail.com>
2014-07-28 09:51:08 +0000

    add city hash with unit tests

--- pyhash.py
+++ pyhash.py
@@ -29,159 +29,149 @@ lookup3_big = _pyhash.lookup3_big
 
 super_fast_hash = _pyhash.super_fast_hash
 
+city_64 = _pyhash.city_64
+city_128 = _pyhash.city_128
+
 import unittest
 import logging
-
 
 class TestHasher(unittest.TestCase):
     def setUp(self):
         self.data = b'test'
         self.udata = u'test'
 
+    def doTest(self, hasher_type, bytes_hash, seed_hash, unicode_hash):
+        hasher = hasher_type()
+
+        self.assertEqual(bytes_hash, hasher(self.data))
+
+        self.assertEqual(seed_hash, hasher(self.data, seed=bytes_hash))
+
+        self.assertEqual(seed_hash, hasher(self.data, self.data))
+
+        self.assertEqual(unicode_hash, hasher(self.udata))
 
 class TestFNV1(TestHasher):
     def testFNV1_32(self):
-        hasher = fnv1_32()
-
-        self.assertEqual(3698262380L, hasher(self.data))
-        self.assertEqual(660137056, hasher(self.data, self.data))
-        self.assertEqual(660137056, hasher(self.data, seed=3698262380L))
-
-        self.assertEqual(3910690890L, hasher(self.udata))
+        self.doTest(hasher_type=fnv1_32,
+                    bytes_hash=3698262380L,
+                    seed_hash=660137056,
+                    unicode_hash=3910690890L)
 
     def testFNV1a_32(self):
-        hasher = fnv1a_32()
-
-        self.assertEqual(1858026756, hasher(self.data))
-        self.assertEqual(1357873952, hasher(self.data, self.data))
-
-        self.assertEqual(996945022, hasher(self.udata))
+        self.doTest(hasher_type=fnv1a_32,
+                    bytes_hash=1858026756,
+                    seed_hash=1357873952,
+                    unicode_hash=996945022)
 
     def testFNV1_64(self):
-        hasher = fnv1_64()
-
-        self.assertEqual(17151984479173897804L, hasher(self.data))
-        self.assertEqual(6349570372626520864L, hasher(self.data, self.data))
-        self.assertEqual(6349570372626520864L, hasher(self.data, seed=17151984479173897804L))
-
-        self.assertEqual(14017453969697934794L, hasher(self.udata))
+        self.doTest(hasher_type=fnv1_64,
+                    bytes_hash=17151984479173897804L,
+                    seed_hash=6349570372626520864L,
+                    unicode_hash=14017453969697934794L)
 
     def testFNV1a_64(self):
-        hasher = fnv1a_64()
-
-        self.assertEqual(11830222609977404196L, hasher(self.data))
-        self.assertEqual(8858165303110309728L, hasher(self.data, self.data))
-
-        self.assertEqual(14494269412771327550L, hasher(self.udata))
+        self.doTest(hasher_type=fnv1a_64,
+                    bytes_hash=11830222609977404196L,
+                    seed_hash=8858165303110309728L,
+                    unicode_hash=14494269412771327550L)
 
 
-class TestMurMurHash2(TestHasher):
+class TestMurMurHash(TestHasher):
     def testMurMurHash1_32(self):
-        hasher = murmur1_32()
-
-        self.assertEqual(1706635965, hasher(self.data))
-        self.assertEqual(1637637239, hasher(self.data, self.data))
-
-        self.assertEqual(2296970802L, hasher(self.udata))
+        self.doTest(hasher_type=murmur1_32,
+                    bytes_hash=1706635965,
+                    seed_hash=1637637239,
+                    unicode_hash=2296970802L)
 
     def testMurMurHash1Aligned_32(self):
-        hasher = murmur1_aligned_32()
-
-        self.assertEqual(1706635965, hasher(self.data))
-        self.assertEqual(1637637239, hasher(self.data, self.data))
-
-        self.assertEqual(2296970802L, hasher(self.udata))
+        self.doTest(hasher_type=murmur1_aligned_32,
+                    bytes_hash=1706635965,
+                    seed_hash=1637637239,
+                    unicode_hash=2296970802L)
 
     def testMurMurHash2_32(self):
-        hasher = murmur2_32()
-
-        self.assertEqual(403862830, hasher(self.data))
-        self.assertEqual(1257009171, hasher(self.data, self.data))
-
-        self.assertEqual(2308212514L, hasher(self.udata))
+        self.doTest(hasher_type=murmur2_32,
+                    bytes_hash=403862830,
+                    seed_hash=1257009171,
+                    unicode_hash=2308212514L)
 
     def testMurMurHash2a_32(self):
-        hasher = murmur2a_32()
-
-        self.assertEqual(1026673864, hasher(self.data))
-        self.assertEqual(3640713775L, hasher(self.data, self.data))
-
-        self.assertEqual(3710634486L, hasher(self.udata))
+        self.doTest(hasher_type=murmur2a_32,
+                    bytes_hash=1026673864,
+                    seed_hash=3640713775L,
+                    unicode_hash=3710634486L)
 
     def testMurMurHash2Aligned32(self):
-        hasher = murmur2_aligned_32()
-
-        self.assertEqual(403862830, hasher(self.data))
-        self.assertEqual(1257009171, hasher(self.data, self.data))
-
-        self.assertEqual(2308212514L, hasher(self.udata))
+        self.doTest(hasher_type=murmur2_aligned_32,
+                    bytes_hash=403862830,
+                    seed_hash=1257009171,
+                    unicode_hash=2308212514L)
 
     def testMurMurHash2Neutral32(self):
-        hasher = murmur2_neutral_32()
-
-        self.assertEqual(403862830, hasher(self.data))
-        self.assertEqual(1257009171, hasher(self.data, self.data))
-
-        self.assertEqual(2308212514L, hasher(self.udata))
+        self.doTest(hasher_type=murmur2_neutral_32,
+                    bytes_hash=403862830,
+                    seed_hash=1257009171,
+                    unicode_hash=2308212514L)
 
     def testMurMurHash2_x64_64a(self):
-        hasher = murmur2_x64_64a()
-
-        self.assertEqual(3407684658384555107L, hasher(self.data))
-        self.assertEqual(14278059344916754999L, hasher(self.data, self.data))
-
-        self.assertEqual(9820020607534352415L, hasher(self.udata))
+        self.doTest(hasher_type=murmur2_x64_64a,
+                    bytes_hash=3407684658384555107L,
+                    seed_hash=14278059344916754999L,
+                    unicode_hash=9820020607534352415L)
 
     def testMurMurHash2_x86_64b(self):
-        hasher = murmur2_x86_64b()
-
-        self.assertEqual(1560774255606158893L, hasher(self.data))
-        self.assertEqual(11567531768634065834L, hasher(self.data, self.data))
-
-        self.assertEqual(7104676830630207180L, hasher(self.udata))
+        self.doTest(hasher_type=murmur2_x86_64b,
+                    bytes_hash=1560774255606158893L,
+                    seed_hash=11567531768634065834L,
+                    unicode_hash=7104676830630207180L)
 
     def testMurMurHash3_32(self):
-        hasher = murmur3_32()
-
-        self.assertEqual(3127628307, hasher(self.data))
-        self.assertEqual(1973649836, hasher(self.data, self.data))
-
-        self.assertEqual(1351191292, hasher(self.udata))
+        self.doTest(hasher_type=murmur3_32,
+                    bytes_hash=3127628307,
+                    seed_hash=1973649836,
+                    unicode_hash=1351191292)
 
     def testMurMurHash3_x86_128(self):
-        hasher = murmur3_x86_128()
-
-        self.assertEqual(113049230771270950235709929058346397488L, hasher(self.data))
-        self.assertEqual(201730919445129814667855021331871906456L, hasher(self.data, self.data))
-
-        self.assertEqual(34467989874860051826961972957664456325L, hasher(self.udata))
+        self.doTest(hasher_type=murmur3_x86_128,
+                    bytes_hash=113049230771270950235709929058346397488L,
+                    seed_hash=201730919445129814667855021331871906456L,
+                    unicode_hash=34467989874860051826961972957664456325L)
 
     def testMurMurHash3_x64_128(self):
-        hasher = murmur3_x64_128()
-
-        self.assertEqual(204797213367049729698754624420042367389L, hasher(self.data))
-        self.assertEqual(25000065729391260169145522623652811022L, hasher(self.data, self.data))
-
-        self.assertEqual(301054382688326301269845371608405900524L, hasher(self.udata))
+        self.doTest(hasher_type=murmur3_x64_128,
+                    bytes_hash=204797213367049729698754624420042367389L,
+                    seed_hash=25000065729391260169145522623652811022L,
+                    unicode_hash=301054382688326301269845371608405900524L)
 
 class TestLookup3(TestHasher):
     def testLookup3(self):
-        hasher = lookup3()
-
-        self.assertEqual(3188463954L, hasher(self.data))
-        self.assertEqual(478901866, hasher(self.data, self.data))
-
-        self.assertEqual(1380664715, hasher(self.udata))
+        self.doTest(hasher_type=lookup3,
+                    bytes_hash=3188463954L,
+                    seed_hash=478901866,
+                    unicode_hash=1380664715)
 
 
 class TestSuperFastHash(TestHasher):
     def testSuperFastHash(self):
-        hasher = super_fast_hash()
+        self.doTest(hasher_type=super_fast_hash,
+                    bytes_hash=942683319,
+                    seed_hash=777359542,
+                    unicode_hash=1430748046)
 
-        self.assertEqual(942683319, hasher(self.data))
-        self.assertEqual(777359542, hasher(self.data, self.data))
 
-        self.assertEqual(1430748046, hasher(self.udata))
+class TestCityHash(TestHasher):
+    def testCityHash64(self):
+        self.doTest(hasher_type=city_64,
+                    bytes_hash=17703940110308125106L,
+                    seed_hash=8806864191580960558L,
+                    unicode_hash=7557950076747784205L)
+
+    def testCityHash128(self):
+        self.doTest(hasher_type=city_128,
+                    bytes_hash=195179989828428219998331619914059544201L,
+                    seed_hash=206755929755292977387372217469167977636L,
+                    unicode_hash=211596129097514838244042408160146499227L)
 
 if __name__ == '__main__':
     if "-v" in sys.argv:
