041113b15b6c8ebd6e3b12fd83746ee2e2927de7
flier.lu@gmail.com <flier.lu@gmail.com@3c9281d8-cbc6-11de-a774-41d3a1f294e1>
2014-07-28 07:43:48 +0000

    upgrade setuptools and remove distribute library

--- trunk/setup.py
+++ trunk/setup.py
@@ -4,20 +4,14 @@ import sys
 import os
 import math
 
-is_py3k = sys.version_info[0] > 2
-
-if is_py3k:
-    import distribute_setup
-    distribute_setup.use_setuptools()
-else:
-    import ez_setup
-    ez_setup.use_setuptools()
+from ez_setup import use_setuptools
+use_setuptools()
 
 from setuptools import setup, Extension
 
 libraries = {
     'fnv': ['hash_32.c', 'hash_32a.c', 'hash_64.c', 'hash_64a.c'],
-    'MurmurHash': ['MurmurHash1.cpp', 'MurmurHash2.cpp', 'MurmurHash3.cpp'],
+    'smhasher': ['MurmurHash1.cpp', 'MurmurHash2.cpp', 'MurmurHash3.cpp'],
     'lookup3': ['lookup3.c'],
     'SuperFastHash': ['SuperFastHash.c'],
 }
@@ -26,7 +20,7 @@ source_files = [os.path.join('src', file) for file in ['Hash.cpp']]
 
 for lib, files in libraries.items():
     source_files += [os.path.join('src', lib, file) for file in files]
-    
+
 macros = [
     ("BOOST_PYTHON_STATIC_LIB", None),
 ]
@@ -50,7 +44,7 @@ if os.name == "nt":
     library_dirs += [
         os.path.join(os.environ.get('BOOST_HOME'), 'stage/lib'),
         os.path.join(os.environ.get('PYTHON_HOME'), 'libs'),
-    ]  
+    ]
 
     extra_compile_args += ["/O2", "/GL", "/MT", "/EHsc", "/Gy", "/Zi"]
     extra_link_args += ["/DLL", "/OPT:REF", "/OPT:ICF", "/MACHINE:X64" if is_64bit else "/MACHINE:X86"]
@@ -74,13 +68,6 @@ pyhash = Extension(name="_pyhash",
                    extra_link_args=extra_link_args,
                    )
 
-extra = {}
-
-if is_py3k:
-    extra.update({
-        'use_2to3': True
-    })
-
 setup(name='pyhash',
     version='0.5.0',
     description='Python Non-cryptographic Hash Library',
@@ -101,7 +88,7 @@ setup(name='pyhash',
         'License :: OSI Approved :: Apache Software License',
         'Natural Language :: English',
         'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX', 
+        'Operating System :: POSIX',
         'Programming Language :: C++',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
@@ -109,4 +96,6 @@ setup(name='pyhash',
         'Topic :: Software Development',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Utilities'
-    ], **extra)
+    ],
+    test_suite='pyhash',
+    use_2to3=True)
