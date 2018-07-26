70bd44977b0107332769927647325ed1e07cac29
Flier Lu <flier.lu@gmail.com>
2018-07-26 15:17:33 +0800

    add test case for lookup3_big

--- tests/test_lookup3.py
+++ tests/test_lookup3.py
@@ -8,3 +8,10 @@ def test_lookup3(hash_tester):
                 bytes_hash=3188463954,
                 seed_hash=478901866,
                 unicode_hash=1380664715)
+
+
+def test_lookup3_big(hash_tester):
+    hash_tester(hasher_type=pyhash.lookup3_big,
+                bytes_hash=305759528,
+                seed_hash=1889773852,
+                unicode_hash=1487153094)
